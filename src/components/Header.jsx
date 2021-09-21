fcbeb6f636e27339f163a563a761984d2c875a67
Alison Lee <alison2686@gmail.com>
2021-09-21 10:11:21 -0700

    page setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import { link } from 'react-router-dom';
+import background from '../images/travel_background.jpg';
+import logo from '../images/elan_logo_black_background.png';
+import '../css/Header.scss'
+ 
 const Header = () => {
     return(
-        <div class='header'>Header</div>
+        <div class='header' style={{ backgroundImage: `url(${background})` }}>
+            <div className='logo-container'>
+                <img src={logo} alt='Elan Travel Services'/>
+            </div>
+            <ul className="nav">
+                <li className="nav-logo">Elan Travel</li>
+                {/* <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/About'>About</Link>
+                </li>
+                <li>
+                    <Link to='/Contact'>Contact</Link>
+                </li> */}
+            </ul>
+        </div>
     )
 }
 
-export default Header
+export default Header;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 const Homepage = () => {
     return(
         <div>
-            <h1>Elan Travel</h1>
+            {/* <h1>Elan Travel</h1> */}
             <Header />
         </div>
     )
